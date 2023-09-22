6aaa997393d454ea73a13542514eeb05124af6ca
Casey Summers <casey.summers@my.jcu.edu.au>
2023-09-22 16:40:20 +1000

    Add function doc strings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,16 +4,19 @@ minimum_length = 3
 
 
 def main():
+    """Gets valid password and converts characters to asterisk"""
     password = get_password()
     print_password(password)
 
 
 def print_password(password):
+    """Gets valid password"""
     for character in range(len(password)):
         print("*", end="")
 
 
 def get_password():
+    """Uses valid password to print equal length of asterisks"""
     password = str(input("Password: "))
     while len(password) < minimum_length:
         print("Invalid password.")
