2c9f280bbac1d4154ccef06a92024e15f55cf31c
Casey Summers <casey.summers@my.jcu.edu.au>
2023-10-06 19:40:20 +1000

    print lists into lists and format printing

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,11 +8,13 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print(data)  # display list input for function below
+    display_subject_details(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    final_list = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -23,7 +25,16 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        final_list.append(parts)
     input_file.close()
+    return final_list
+
+
+def display_subject_details(data):
+    counter = 0
+    for datapoint in data:
+        print(f"{data[counter][0]} is taught by {data[counter][1]} and has {data[counter][2]} students")
+        counter += 1
 
 
 main()
