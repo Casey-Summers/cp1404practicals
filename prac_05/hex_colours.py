626c746404f6f9c4affa1afb978f023c5da79e28
Casey Summers <casey.summers@my.jcu.edu.au>
2023-10-12 18:29:58 +1000

    Complete hex colour program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,5 +1,19 @@
 """
 CP1404 Practical
-Hex colours in key dictionary
-File needs reformatting
+Hex colours in CONSTANT dictionary
+Name: Casey Summers
 """
+
+TEN_COLOURS_TO_CODES = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff",
+                        "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
+                        "Antique White0": "#faebd7", "Antique White1": "#ffefdb", "Antique White2": "#eedfcc"}
+# I have spaced colours that had two capitalised words put together for user consistency.
+
+colour_choice = input("What colour: ").title()
+while colour_choice != "":
+    if colour_choice in TEN_COLOURS_TO_CODES:
+        print(f"The colour code for {colour_choice} is: {TEN_COLOURS_TO_CODES[colour_choice]}")
+    else:
+        print("Invalid.")
+    colour_choice = input("What colour: ").title()
+print("Thank you for trying!")
