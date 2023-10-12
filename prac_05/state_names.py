9bb6bd46658b6dd4af62797fc9442234fd0f269e
Casey Summers <casey.summers@my.jcu.edu.au>
2023-10-13 00:34:40 +1000

    Add task 4 to print all state names with formatting

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,13 +8,19 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
+# print all state name with formatting
+max_width = max(len(state) for state in CODE_TO_NAME)
+for codes, states in CODE_TO_NAME.items():
+    print(f"{codes:{max_width}} is {states} ")
+
+# ask for state to validate and print
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     try:
         print(state_code, "is", CODE_TO_NAME[state_code])
     except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+    state_code = input("Enter short state: ").upper
 
     # LBYL approach
     # if state_code in CODE_TO_NAME:
