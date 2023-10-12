110539e062276e98ff6278b6336c3ef38701dc9f
Casey Summers <casey.summers@my.jcu.edu.au>
2023-10-13 02:24:26 +1000

    Update hex colours with .replace()

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,9 +7,8 @@ Name: Casey Summers
 TEN_COLOURS_TO_CODES = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff",
                         "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
                         "Antique White0": "#faebd7", "Antique White1": "#ffefdb", "Antique White2": "#eedfcc"}
-# I have spaced colours that had two capitalised words put together for user consistency.
 
-colour_choice = input("What colour: ").title()
+colour_choice = input("What colour: ").title().replace(" ", "")
 while colour_choice != "":
     if colour_choice in TEN_COLOURS_TO_CODES:
         print(f"The colour code for {colour_choice} is: {TEN_COLOURS_TO_CODES[colour_choice]}")
