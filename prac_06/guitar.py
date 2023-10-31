9da659e2bbf9335f128115a67effc7b06d57a3c9
Casey Summers <casey.summers@my.jcu.edu.au>
2023-10-31 18:42:56 +1000

    Correct default cases

---
+++ prac_06/guitar.py
@@ -0,0 +1,7 @@
+"""
+Name: Casey Summers
+CP1404 Practical - Guitar class.
+"""
+
+class Guitar:
+    def __init__(self, name="", year=0.0):
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -5,7 +5,7 @@ CP1404 Practical - Programming Languages class.
 
 
 class ProgrammingLanguage:
-    def __init__(self, name="", typing="", reflection="", year=""):
+    def __init__(self, name="", typing="", reflection=False, year=0.0):
         self.name = name
         self.typing = typing
         self.reflection = reflection
