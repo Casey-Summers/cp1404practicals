eddfb5e66d4bcecdbdaefc3cd633c94781e240e3
Casey Summers <casey.summers@my.jcu.edu.au>
2023-10-31 19:16:19 +1000

    Add tests for all class methods and correct issues

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,25 @@
+"""
+CP1404 Practical - Client code to use the guitar class.
+Name: Casey Summers
+Estimated time: 25m
+Actual time: 15m
+"""
+
+from prac_06.guitar import Guitar
+
+
+def main():
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another_guitar = Guitar("Another Guitar", 2013, 800.00)
+    print(f"{gibson}\n")  # Test f-string formatting
+
+    # Test get_age() method
+    print(f"{gibson.name} get_age() - Expected 101. Got {gibson.get_age()}")
+    print(f"{another_guitar.name} get_age() - Expected 10. Got {another_guitar.get_age()}")
+
+    # Test is_vintage() method
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected False. Got {another_guitar.is_vintage()}")
+
+
+main()
