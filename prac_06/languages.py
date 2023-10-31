f0b0ba3ed0d3551718adea1593b23264f7a48e2b
Casey Summers <casey.summers@my.jcu.edu.au>
2023-10-31 19:56:01 +1000

    Add docstring

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -9,6 +9,7 @@ from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
+    """Program to test ProgrammingLanguages class and to display existing dynamic languages."""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
