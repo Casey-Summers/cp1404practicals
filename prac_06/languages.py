63b408c0ff9a90e024298a8c3fb9f0287a34aebe
Casey Summers <casey.summers@my.jcu.edu.au>
2023-10-31 18:16:47 +1000

    Add programming language examples to test class with, prints python example

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+"""
+CP1404 Practical - Client code to use the ProgrammingLanguages class.
+Name: Casey Summers
+Estimated time: 35m
+Actual time:
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
