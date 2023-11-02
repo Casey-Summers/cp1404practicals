bf1cd2afd32aa9c265f56ce116edd798824ebca1
Casey Summers <casey.summers@my.jcu.edu.au>
2023-11-02 23:20:24 +1000

    Complete add guitar functionality and saving back into the .csv

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,7 +2,7 @@
 CP1404 - Guitar file reader and display
 Name: Casey Summers
 Expected time: 45m
-Actual time:
+Actual time: 65m
 """
 
 from guitar import Guitar
@@ -16,9 +16,21 @@ def main():
     guitars = load_guitars_from_csv(FILENAME)
     guitars.sort()
     print("Guitars List.")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:.2f} added.")
+        name = input(f"\nName: ")
+
+    # Display Guitar list
     for i, guitar in enumerate(guitars, 1):
         vintage_string = "(vintage)" if guitar.is_vintage() else ""
         print(f"Guitar {i}: {guitar} {vintage_string}")
+
+    save_guitars_to_file(guitars)
+    print(f"\nGuitars saved to {FILENAME}")
 
 
 def load_guitars_from_csv(filename):
@@ -36,6 +48,13 @@ def load_guitars_from_csv(filename):
             except IndexError:
                 print("Invalid index.")
         return guitars
+
+
+def save_guitars_to_file(guitars):
+    """Save lists of class objects back into the file with the same format."""
+    with open(FILENAME, 'w') as out_file:
+        for guitar in guitars:
+            out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
 
 
 if __name__ == '__main__':
